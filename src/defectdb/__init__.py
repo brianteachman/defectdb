60619a9a3311913b0ada9f263b81e881771fbd14
mrbtx <mr.teachman@gmail.com>
2021-12-19 14:53:26 -0800

    Edited namespace in attempt to get it right.

--- src/defectdb/__init__.py
+++ src/defectdb/__init__.py
@@ -4,7 +4,7 @@ defectdb.
 An SQLite database abstraction layer (DAL) loosely based on the Object Relational Mapper (ORM) pattern.
 """
 
-from defectdb import DefectManager, Defect, DefectSchema
+from defectdb.defectmanager import DefectManager, Defect, DefectSchema
 
 __version__ = "0.1.0"
 __author__ = 'Brian Teachman'
